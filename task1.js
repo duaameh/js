9018950ffa907c659c2e2b4f6475e6873669f7e9
duaameh <mehdawiduaa@gmail.com>
2024-06-05 16:36:10 +0300

    Add files via upload

--- task1.js
+++ task1.js
@@ -1,29 +1,11 @@
-document.getElementById('calForm')
-.addEventListener('submit', function(event) {
-    event.preventDefault();
+let x=parseInt(prompt('value x'));
 
-
-   
-    let operation = document.getElementById('operation').value;
-    let num1 = parseFloat(document.getElementById('num1').value);
-    let num2 = parseFloat(document.getElementById('num2').value);
-
-    let result;
-   
-    if (operation === "+") {
-        result = num1 + num2;
-    } else if (operation === "-") {
-        result = num1 - num2;
-    } else if (operation === "*") {
-        result = num1 * num2;
-    } else if (operation === "/") {
-        result = num1 / num2;
-    } else {
-        result = "Invalid operation";
+let value;
+if(x%2==0)
+    {
+        value="EVEN"
     }
-
-    
-    console.log(`Operation: ${operation}, Numbers: ${num1} and ${num2}, Result: ${result}`);
-});
-
-
+else {
+    value="odd"
+}
+alert(value);
